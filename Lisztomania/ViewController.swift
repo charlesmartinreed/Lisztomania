6621e0a235b1bc8d77304b299eb8bc77a4875e9d
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-09-17 12:24:08 -0500

    Basic version. Audio scrubbing not yet working.

--- Lisztomania/ViewController.swift
+++ Lisztomania/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    //MARK:- @IBOutlets
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var scrubbingSlider: UISlider!
+    
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //try finding the audio file
+        if let audioPath = Bundle.main.path(forResource: "hungarian", ofType: "mp3") {
+            //if you can find the file, try loading it into our audio player
+            do {
+                try audioPlayer = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath))
+                audioPlayer.volume = 0.5
+            } catch {
+                print("could not play back file")
+            }
+        }
     }
+    
+    @IBAction func playButtonTapped(_ sender: Any) {
+        audioPlayer.play()
+    }
+    
 
-
+    @IBAction func pauseButtonTapped(_ sender: Any) {
+        audioPlayer.pause()
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+        audioPlayer.stop()
+        
+        //with this line, stopping the song will cause it to return to the beginning at next play.
+        audioPlayer.currentTime = 0
+    }
+    
+    
+    @IBAction func volumeChanged(_ sender: UISlider) {
+        audioPlayer.volume = (volumeSlider.value / 100)
+    }
+    
+    @IBAction func scrubbedChanged(_ sender: UISlider) {
+    }
+    
+    
 }
 
